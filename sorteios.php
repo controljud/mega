2b5a6c418845a2fb7935316d18bd43875b8310bd
Isaias Santos <isaias.santos@veus.com.br>
2018-12-03 15:48:25 -0200

    Atualização

--- sorteios.php
+++ sorteios.php
@@ -32,37 +32,37 @@
     ksort($sorteados);
 ?>
 <html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-</head>
-<body>
-<dv class="row">
-    <dv class="col-md-3"></dv>
-    <dv class="col-md-6">
-        <h3>Dezenas mais sorteadas da Mega Sena (Mapa de calor)</h3>
-        <table class="table table-bordered tbl-principal">
-            <?php
-            $x = 0;
-            foreach($sorteados as $key => $sorteado){
-                if($x == 0){
-                    echo "<tr>";
-                }else if($x == 10){
-                    $x = 0;
-                    echo "</tr>";
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </head>
+    <body>
+    <dv class="row">
+        <dv class="col-md-3"></dv>
+        <dv class="col-md-6">
+            <h3>Dezenas mais sorteadas da Mega Sena (Mapa de calor)</h3>
+            <table class="table table-bordered tbl-principal">
+                <?php
+                $x = 0;
+                foreach($sorteados as $key => $sorteado){
+                    if($x == 0){
+                        echo "<tr>";
+                    }else if($x == 10){
+                        $x = 0;
+                        echo "</tr>";
+                    }
+
+                    echo "<td class='center td-numero td-".$sorteado."' id='".$sorteado."'><span class='sp-numero'>".$key."</span><br/>".$sorteado."</td>";
+
+                    $x++;
                 }
-
-                echo "<td class='center td-numero td-".$sorteado."' id='".$sorteado."'><span class='sp-numero'>".$key."</span><br/>".$sorteado."</td>";
-
-                $x++;
-            }
-            ?>
-        </table>
+                ?>
+            </table>
+        </dv>
+        <dv class="col-md-3"></dv>
     </dv>
-    <dv class="col-md-3"></dv>
-</dv>
-</body>
+    </body>
 </html>
